903cd9db2483ad8ce691fbb66e73cb27e9d8e539
Julien Broens <julien-broens@MBP-de-Julien.lan>
2021-12-16 16:54:19 +0100

    change h1 header color

--- components/Header.js
+++ components/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
                 <div class='w-20 pt-2 pb-2 md:w-40'>
                     <img src='./profil-pic-01.jpg'></img>
                 </div>
-                <div class='flex flex-col justify-center flex-auto pl-4 bg-gradient-to-t from-amber-300 via-amber-400 to-amber-500 pt-2 pb-2 md:text-3xl md:font-bold md:text-white md:pl-10'>
+                <div class=' text-white font-bold flex flex-col justify-center flex-auto pl-4 bg-gradient-to-t from-amber-300 via-amber-400 to-amber-500 pt-2 pb-2 md:text-3xl md:pl-10'>
                     <h1>JULIEN BROENS</h1>
                     <h2>Développeur web</h2>
                 </div >
