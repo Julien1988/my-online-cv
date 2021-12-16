3f86257675c886e8e3d015241e8e4363d2c3f800
Julien Broens <julien-broens@MBP-de-Julien.lan>
2021-12-16 16:51:15 +0100

    add responsive media q md & lg

--- components/Header.js
+++ components/Header.js
@@ -7,13 +7,13 @@ const Header = () => {
     return (
         <header className="header">
             <div class='header__container flex flex-row '>
-                <div class='w-20 pt-2'>
+                <div class='w-20 pt-2 pb-2 md:w-40'>
                     <img src='./profil-pic-01.jpg'></img>
                 </div>
-                <div class='flex flex-col justify-center flex-auto pl-4 bg-gradient-to-t from-amber-300 via-amber-400 to-amber-500 pt-2'>
+                <div class='flex flex-col justify-center flex-auto pl-4 bg-gradient-to-t from-amber-300 via-amber-400 to-amber-500 pt-2 pb-2 md:text-3xl md:font-bold md:text-white md:pl-10'>
                     <h1>JULIEN BROENS</h1>
                     <h2>Développeur web</h2>
-                </div>
+                </div >
             </div>
            
   
--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,11 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className='pl-4 pr-4'>
-      <div class='details flex flex-col'>
+    <main className='pl-4 pr-4 pb-6 md:pt-4'>
+      <div class='details flex flex-col lg:pb-6'>
         <h1 class='bg-slate-800 text-white uppercase pl-1 pr-1 mt-3 w-fit'>Détails</h1>
-        <div>
-          <ul>
+        <div class='md:flex md:flex-row md:flex-wrap'>
+          <ul class='md:pr-16'>
             <li><h2 class='text-sm font-medium pt-2 pb-1'>Contact</h2></li>
             <li><p class='text-xs'>35, Tienne Jean-Pierre, 5150 FLOREFFE</p></li>
             <li><h2 class='text-sm font-medium pt-2 pb-1'>Nationalité</h2></li>
@@ -24,62 +24,64 @@ export default function Home() {
           </ul>
         </div>
       </div>
-      <div class='experiences flex flex-col'>
-        <h1 class='bg-slate-800 text-white uppercase pl-1 pr-1 mt-3 w-fit'>Expériences profesionnelles</h1>
-        <ul>
-            <li><h2 class='text-sm font-medium pt-2 '>Educateur spécialisé, A1 Liège</h2></li>
-            <li><p class='text-xs text-slate-300 pb-1'>2011 - 2018</p></li>
-            <li><p class='text-xs'>Agent éducation en SRJ</p></li>
-        </ul>
-        <ul>
-            <li><h2 class='text-sm font-medium pt-2 '>Développeur web, O'Clock</h2></li>
-            <li><p class='text-xs text-slate-300 pb-1'>2019 - 2020</p></li>
-            <li><p class='text-xs'>Formation de 6 mois dans le développement web (Wordpress, Php, Poo, Mysql, Js, Html, Css)</p></li>
-        </ul>
-        <ul>
-            <li><h2 class='text-sm font-medium pt-2 '>Développeur web, BeCode Liège</h2></li>
-            <li><p class='text-xs text-slate-300 pb-1'>2020 - 2020</p></li>
-            <li><p class='text-xs'>Formation de 6 mois dans le développement web (NodeJS, React, Wordpress, Php, MongoDB, Netlify)</p></li>
-        </ul>
-        <ul>
-            <li><h2 class='text-sm font-medium pt-2 '>Stage développeur web, OANNA Nandrin</h2></li>
-            <li><p class='text-xs text-slate-300 pb-1'>Septembre 2020 - Décembre 2020</p></li>
-            <li><p class='text-xs'>Stage en entreprise de 3 mois dans une agence de communication (Prestashop, Cms made simple, JQuery, Symfony)</p></li>
-        </ul>
-        <ul>
-            <li><h2 class='text-sm font-medium pt-2 '>Educateur spécialisé, A1 Bruxelles</h2></li>
-            <li><p class='text-xs text-slate-300 pb-1'>2021 - ... </p></li>
-            <li><p class='text-xs'>Agent éducation milieu des sans abris</p></li>
-        </ul>
-        <ul>
-            <li><h2 class='text-sm font-medium pt-2 '>Développeur indépendant</h2></li>
-            <li><p class='text-xs text-slate-300 pb-1'>2021 - ... </p></li>
-            <li><p class='text-xs'>Perfectionnement de mes acquis et connaissances</p></li>
-        </ul>
-        
-      </div>
-      <div class='skills flex flex-col'>
-        <h1 class='bg-slate-800 text-white uppercase pl-1 pr-1 mt-3 w-fit'>Compétences</h1>
-        <ul class='pt-2 '>
-          <li><p class='text-xs'>HTML & CSS</p></li>
-          <li><p class='text-xs'>React</p></li>
-          <li><p class='text-xs'>NextJS</p></li>
-          <li><p class='text-xs'>MongoDB</p></li>
-          <li><p class='text-xs'>Mysql</p></li>
-          <li><p class='text-xs'>Strapi</p></li>
-          <li><p class='text-xs'>JavaScript</p></li>
-          <li><p class='text-xs'>Php & POO</p></li>
-          <li><p class='text-xs'>Wordpress</p></li>
-          <li><p class='text-xs'>Prestashop</p></li>
-          <li><p class='text-xs'>TailwindCSS</p></li>
-          <li><p class='text-xs'>Git</p></li>
-          <li><p class='text-xs'>Communication</p></li>
-          <li><p class='text-xs'>Envie d'apprendre</p></li>
-         
-         
+      <div class='lg:flex lg:flex-wrap lg:pb-6'>
+        <div class='experiences flex flex-col lg:pr-8 lg:border-r-2'>
+          <h1 class='bg-slate-800 text-white uppercase pl-1 pr-1 mt-3 w-fit'>Expériences profesionnelles</h1>
+          <ul>
+              <li><h2 class='text-sm font-medium pt-2 '>Educateur spécialisé, A1 Liège</h2></li>
+              <li><p class='text-xs text-slate-300 pb-1'>2011 - 2018</p></li>
+              <li><p class='text-xs'>Agent éducation en SRJ</p></li>
           </ul>
+          <ul>
+              <li><h2 class='text-sm font-medium pt-2 '>Développeur web, O'Clock</h2></li>
+              <li><p class='text-xs text-slate-300 pb-1'>2019 - 2020</p></li>
+              <li><p class='text-xs'>Formation de 6 mois dans le développement web (Wordpress, Php, Poo, Mysql, Js, Html, Css)</p></li>
+          </ul>
+          <ul>
+              <li><h2 class='text-sm font-medium pt-2 '>Développeur web, BeCode Liège</h2></li>
+              <li><p class='text-xs text-slate-300 pb-1'>2020 - 2020</p></li>
+              <li><p class='text-xs'>Formation de 6 mois dans le développement web (NodeJS, React, Wordpress, Php, MongoDB, Netlify)</p></li>
+          </ul>
+          <ul>
+              <li><h2 class='text-sm font-medium pt-2 '>Stage développeur web, OANNA Nandrin</h2></li>
+              <li><p class='text-xs text-slate-300 pb-1'>Septembre 2020 - Décembre 2020</p></li>
+              <li><p class='text-xs'>Stage en entreprise de 3 mois dans une agence de communication (Prestashop, Cms made simple, JQuery, Symfony)</p></li>
+          </ul>
+          <ul>
+              <li><h2 class='text-sm font-medium pt-2 '>Educateur spécialisé, A1 Bruxelles</h2></li>
+              <li><p class='text-xs text-slate-300 pb-1'>2021 - ... </p></li>
+              <li><p class='text-xs'>Agent éducation milieu des sans abris</p></li>
+          </ul>
+          <ul>
+              <li><h2 class='text-sm font-medium pt-2 '>Développeur indépendant</h2></li>
+              <li><p class='text-xs text-slate-300 pb-1'>2021 - ... </p></li>
+              <li><p class='text-xs'>Perfectionnement de mes acquis et connaissances</p></li>
+          </ul>
+          
+        </div>
+        <div class='skills flex flex-col lg:pl-8'>
+          <h1 class='bg-slate-800 text-white uppercase pl-1 pr-1 mt-3 w-fit'>Compétences</h1>
+          <ul class='pt-2 '>
+            <li><p class='text-xs pb-1 lg:pb-2'>HTML & CSS</p></li>
+            <li><p class='text-xs pb-1 lg:pb-2'>React</p></li>
+            <li><p class='text-xs pb-1 lg:pb-2'>NextJS</p></li>
+            <li><p class='text-xs pb-1 lg:pb-2'>MongoDB</p></li>
+            <li><p class='text-xs pb-1 lg:pb-2'>Mysql</p></li>
+            <li><p class='text-xs pb-1 lg:pb-2'>Strapi</p></li>
+            <li><p class='text-xs pb-1 lg:pb-2'>JavaScript</p></li>
+            <li><p class='text-xs pb-1 lg:pb-2'>Php & POO</p></li>
+            <li><p class='text-xs pb-1 lg:pb-2'>Wordpress</p></li>
+            <li><p class='text-xs pb-1 lg:pb-2'>Prestashop</p></li>
+            <li><p class='text-xs pb-1 lg:pb-2'>TailwindCSS</p></li>
+            <li><p class='text-xs pb-1 lg:pb-2'>Git</p></li>
+            <li><p class='text-xs pb-1 lg:pb-2'>Communication</p></li>
+            <li><p class='text-xs pb-1 lg:pb-2'>Envie d'apprendre</p></li>
+          
+          
+            </ul>
+        </div>
       </div>
-      <div class='languages flex flex-col'>
+      <div class='languages flex flex-col lg:pb-6'>
         <h1 class='bg-slate-800 text-white uppercase pl-1 pr-1 mt-3 w-fit'>Langues</h1>
         <ul class='pt-2 '>
           <li><h2 class='text-sm font-medium'>Français</h2></li>
